4f68cf6eab7616a5b777ed152d42eec1b0a824fa
Vitalii Lysenko <balthazar.kain@gmail.com>
2018-04-20 22:08:57 +0300

    Simple Calculator

---
+++ Calculator/Calculator/SimpleCalculator.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Calculator
+{
+    public class SimpleCalculator
+    {  
+        public int Addition(int x, int y)
+        { return x + y; }
+
+        public int Substraction(int x, int y)
+        { return x - y; }
+
+        public int Multiplication(int x, int y)
+        { return x * y; }
+
+        public int Divide(int x, int y)
+        { return x / y; }
+
+       
+    }
+}

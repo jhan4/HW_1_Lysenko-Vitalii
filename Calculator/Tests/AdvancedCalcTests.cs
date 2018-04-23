3f885777d76a336e1ebb2c433c17723cd5350849
Vitalii Lysenko <balthazar.kain@gmail.com>
2018-04-24 00:19:29 +0300

    Update to HW_2

--- Calculator/Tests/AdvancedCalcTests.cs
+++ Calculator/Tests/AdvancedCalcTests.cs
@@ -7,23 +7,61 @@ namespace Tests
     [TestFixture]
     public class AdvancedCalcTests
     {
-        [Test]
+        ScientificCalculator a = new ScientificCalculator();
 
+        [Test, Retry(3)]
         public void ArrayMinValueTesting()
         {
             int[] arr = { 4, 5, 2, 1, 7 };
             int expected = 1;
-            ScientificCalculator a = new ScientificCalculator();
             int actual = a.ArrayMinValue(arr);
             Assert.AreEqual(expected, actual);
         }
 
-        [Test]
+        [Test, Description("Verify Array is not empty")]
+        public void ArrayIsNotEmptyTesting()
+        {
+            int[] arr = { 4, 5, 2, 1, 7 };
+
+            Assert.IsNotEmpty(arr);
+        }
+
+        [Test, ]
+        public void ComparisonArrayTesting()
+        {
+            int[] MaxOfAarray = { 4, 5, 7 };
+            int[] MinOfAarray = { -3, 1, 2 };
+            Assert.That(MinOfAarray, Is.Not.EquivalentTo(MaxOfAarray));
+        }
+
+        [Test,]
+        public void ComparisonArrayTesting2()
+        {
+            int[] MaxOfAarray = { 4, 5, 7 };
+            int[] MinOfAarray = { 4, 5, 7 };
+            Assert.That(MinOfAarray, Is.EquivalentTo(MaxOfAarray));
+        }
+
+        [Test,]
+        public void ComparisonArrayTesting3()
+        {
+            int[] MinOfAarray = { 4, 5, 7 };
+            Assert.That(MinOfAarray, Is.All.InRange(3, 8));
+        }
+
+        [Test, Description("Verify Array is empty")]
+        public void ArrayIsEmptyTesting()
+        {
+            int[] arr = { };
+
+            Assert.IsEmpty(arr);
+        }
+
+        [Test, Repeat(2)]
         public void ArrayMaxValueTesting()
         {
             int[] arr = { 2, 4, 7, 1 };
             int expected = 7;
-            ScientificCalculator a = new ScientificCalculator();
             int actual = a.ArrayMaxValue(arr);
             Assert.AreEqual(expected, actual);
         }
@@ -33,7 +71,6 @@ namespace Tests
         {
             int arg1 = 2;
             int arg2 = 3;
-            ScientificCalculator a = new ScientificCalculator();
             bool actual = a.compare(arg1, arg2);
             Assert.True(actual);
         }
@@ -43,7 +80,6 @@ namespace Tests
         {
             int arg1 = 1;
             int arg2 = 1;
-            ScientificCalculator a = new ScientificCalculator();
             bool actual = a.compare(arg1, arg2);
             Assert.True(actual);
         }
@@ -54,25 +90,56 @@ namespace Tests
         {
             int arg1 = 3;
             int arg2 = 2;
-            ScientificCalculator a = new ScientificCalculator();
             bool actual = a.compare(arg1, arg2);
             Assert.True(actual);
         }
-        [Test]
+        [Test, Description("Testing of Sqrt Method")]
         public void SqrtTesting()
         {
             double d = 25;
             double expected = 5;
             double actual = ScientificCalculator.Sqrt(d);
-            Assert.AreEqual(expected, actual);
+            Assert.AreEqual(expected, actual, "Square root from 25 to be equal to 5");
         }
 
-        [Test]
+        [Test, Description("Assert.Positive Testing")]
+        public void SqrtTesting2()
+        {
+            double d = 25;
+            double actual = ScientificCalculator.Sqrt(d);
+            Assert.Positive(actual, "Actual Result should be positive");
+        }
+
+        [Test, Description("Assert.Greater Testing")]
+        public void SqrtTesting3()
+        {
+            double d = 25;
+            double actual = ScientificCalculator.Sqrt(d);
+            Assert.Greater(actual, 0, "Actual Result should be Greater than 0");
+        }
+
+        [Test, Description("Pow Method Testing")]
         public void PowTesting()
         {
             double x = 5;
             double y = 2;
             double expected = 25;
+            double actual = ScientificCalculator.Pow(x, y);
+            Assert.AreEqual(expected, actual, "5^2 should be equal to 25");
+        }
+
+        [Test, Description("Assert.Positive Testing")]
+        public void PowTesting2()
+        {
+            double actual = ScientificCalculator.Pow(2, -8);
+            Assert.That(actual, Is.Positive, "Actual Result should be positive");
+        }
+        [Test, Ignore("Test should be ignored")]
+        public void IgnoringOfPowTesting()
+        {
+            double x = -2;
+            double y = 6;
+            double expected = -64;
             double actual = ScientificCalculator.Pow(x, y);
             Assert.AreEqual(expected, actual);
         }
@@ -87,15 +154,113 @@ namespace Tests
             int actual = a.Mod(x, y);
             Assert.AreEqual(expected, actual);
         }
+
+        [Test, Description("Assert.Less Testing")]
+        public void ModTesting2()
+        {
+            int x = 25;
+            int y = 7;
+            ScientificCalculator a = new ScientificCalculator();
+            int actual = a.Mod(x, y);
+            Assert.Less(actual, 5, "Actual result should be less than 5");
+        }
         [Test]
         public void PercentTesting()
         {
             double x = 50;
             double y = 5;
             double expected = 2.5;
-
             double actual = ScientificCalculator.Percent(x, y);
             Assert.AreEqual(expected, actual);
         }
+
+        [Test(ExpectedResult = 2.5)]
+        public double PercentTesting2()
+        {
+            return ScientificCalculator.Percent(50, 5);
+        }
+
+        [TestCase(32, 8)]
+        [TestCase(16, 4)]
+        [TestCase(-24, -6)]
+        public void DivideTests(int a, int b)
+        {
+            Assert.GreaterOrEqual(a / b, 4);
+        }
+
+        [TestCase(3, 9, 3)]
+        [TestCase(3, 18, 6)]
+        [TestCase(3, -24, 8)]
+        public void Divide2Tests(int expected, int a, int b)
+        {
+
+            Assert.AreNotEqual(expected, (a / b) * 2);
+        }
+
+        [Test]
+        public void TestPassed()
+        { Assert.Pass("Test passed"); }
+
+        [Test]
+        public void TestFailed()
+        { Assert.Fail("Test Failed"); }  
     }
-}
+
+
+    [TestFixture, Description("String Assert Testting")]
+
+    public class StringTesting
+    {
+        [Test]
+        public void AssertStringContain()
+        {
+            Assert.That("the best", Does.Contain("the best"), "If you want to be the best...");
+        }
+
+        [Test]
+        public void AssertStringEnd()
+        {
+            Assert.That("the best", Does.EndWith("st"), "If you want to be the best...");
+        }
+
+        [Test]
+        public void AssertStringStart()
+        {
+            Assert.That("the best", Does.StartWith("th"), "If you want to be the best...");
+        }
+
+        [Test]
+        public void AssertIgnore()
+        {
+            StringAssert.AreEqualIgnoringCase("The Best", "the best", "If you want to be the best...");
+        }
+    }
+
+    [TestFixture, Description("SetUp and TearDown Testing")]
+
+    public class FixtureSetupTesting
+    {
+        ScientificCalculator a;
+        [SetUp]
+        public void Construct()
+        {
+            a = new ScientificCalculator();
+        }
+
+        [Test]
+        public void SqrtTesting4()
+        {
+            double d = 16;
+            double actual = ScientificCalculator.Sqrt(d);
+            Assert.AreEqual(actual, 4);
+        }
+
+        [TearDown]
+        public void CleanUp()
+        {
+            a = null;
+        }
+    }
+ }
+
+
--- Calculator/Tests/SimpleCalcTests.cs
+++ Calculator/Tests/SimpleCalcTests.cs
@@ -4,64 +4,91 @@ using NUnit.Framework;
 
 namespace Tests
 {
-    [TestFixture]
+    [TestFixture, Description("Unit Tests for Simple Calculator")]
     public class SimpleCalcTests
     {
-        [Test]
+        SimpleCalculator a = new SimpleCalculator();
+        [Test, Ignore("Negative Test")]
         public void NotEqual1Testing()
         {
             int x = 8;
             int y = 5;
             int expected = 14;
-
-            SimpleCalculator a = new SimpleCalculator();
             int actual = a.Addition(x, y);
             Assert.AreNotEqual(expected, actual);
         }
-        [Test]
+        [Test, Order(3)]
         public void AdditiongTesting()
         {
             int x = 8;
             int y = 5;
             int expected = 13;
-
-            SimpleCalculator a = new SimpleCalculator();
-                int actual = a.Addition(x, y);
+            int actual = a.Addition(x, y);
             Assert.AreEqual(expected, actual);
         }
 
-        [Test]
+        [Test, Order(4)]
         public void SubstractionTesting()
         {
             int x = 8;
             int y = 5;
             int expected = 3;
-
-            SimpleCalculator a = new SimpleCalculator();
             int actual = a.Substraction(x, y);
             Assert.AreEqual(expected, actual);
         }
-        [Test]
+        [Test, Order(1)]
         public void MultiplicationTesting()
         {
             int x = 2;
             int y = 6;
             int expected = 12;
-
-            SimpleCalculator a = new SimpleCalculator();
             int actual = a.Multiplication(x, y);
             Assert.AreEqual(expected, actual);
         }
-        [Test]
+
+        [Test, Description("Assert.Zero Testing")]
+        public void MultiplicationTesting3()
+        {
+            int num1 = 2;
+            int num2 = 0;
+            int actual = a.Multiplication(num1, num2);
+            Assert.Zero(actual, "Actual Result should be equal to 0");
+        }
+        [Test, Order(2)]
         public void DivideTesting()
         {
             int x = 12;
             int y = 4;
             int expected = 3;
-
-            SimpleCalculator a = new SimpleCalculator();
             int actual = a.Divide(x, y);
             Assert.AreEqual(expected, actual);
+        }
+
+        [Test, Description ("Assert.Negative Testing")]
+        public void DivideTesting3()
+        {
+            int x = 12;
+            int y = -4;
+            int actual = a.Divide(x, y);
+            Assert.Negative(actual, "Actual Result should be negative");
+        }
+
+        [Test (ExpectedResult=4)]
+        public int DivideTesting2()
+        {
+            return 12 / 3;         
+        }
+
+        [Test(ExpectedResult = -8)]
+        public int MultiplicationTesting2()
+        {
+            return -2 * 4;
+        }
+
+        [Test (ExpectedResult =-10)]
+        public int SubstractionTesting2()
+        {
+            return -7 - 3;
         }
     }
 }

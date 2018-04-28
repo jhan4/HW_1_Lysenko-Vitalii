5bbc4551a35e30a0f51cc26e80709e797f2ac6b6
Vitalii Lysenko <balthazar.kain@gmail.com>
2018-04-28 10:24:28 +0300

    Update of Simple Calc Tests

--- Calculator/Tests/SimpleCalcTests.cs
+++ Calculator/Tests/SimpleCalcTests.cs
@@ -17,6 +17,7 @@ namespace Tests
             int actual = a.Addition(x, y);
             Assert.AreNotEqual(expected, actual);
         }
+
         [Test, Order(3)]
         public void AdditiongTesting()
         {
@@ -39,56 +40,56 @@ namespace Tests
         [Test, Order(1)]
         public void MultiplicationTesting()
         {
-            int x = 2;
-            int y = 6;
-            int expected = 12;
-            int actual = a.Multiplication(x, y);
+            double x = 2;
+            double y = 6;
+            double expected = 12;
+            double actual = a.Multiplication(x, y);
             Assert.AreEqual(expected, actual);
         }
 
         [Test, Description("Assert.Zero Testing")]
         public void MultiplicationTesting3()
         {
-            int num1 = 2;
-            int num2 = 0;
-            int actual = a.Multiplication(num1, num2);
+            double num1 = 2;
+            double num2 = 0;
+            double actual = a.Multiplication(num1, num2);
             Assert.Zero(actual, "Actual Result should be equal to 0");
         }
         [Test, Order(2)]
         public void DivideTesting()
         {
-            int x = 12;
-            int y = 4;
-            int expected = 3;
-            int actual = a.Divide(x, y);
+            double x = 12;
+            double y = 4;
+            double expected = 3;
+            double actual = a.Divide(x, y);
             Assert.AreEqual(expected, actual);
         }
 
         [Test, Description ("Assert.Negative Testing")]
         public void DivideTesting3()
         {
-            int x = 12;
-            int y = -4;
-            int actual = a.Divide(x, y);
+            double x = 12;
+            double y = -4;
+            double actual = a.Divide(x, y);
             Assert.Negative(actual, "Actual Result should be negative");
         }
 
         [Test (ExpectedResult=4)]
-        public int DivideTesting2()
+        public double DivideTesting2()
         {
-            return 12 / 3;         
+            return a.Divide(12, 3);         
         }
 
         [Test(ExpectedResult = -8)]
-        public int MultiplicationTesting2()
+        public double MultiplicationTesting2()
         {
-            return -2 * 4;
+            return a.Multiplication(- 2, 4);
         }
 
         [Test (ExpectedResult =-10)]
         public int SubstractionTesting2()
         {
-            return -7 - 3;
+            return a.Substraction(- 7, 3);
         }
     }
 }

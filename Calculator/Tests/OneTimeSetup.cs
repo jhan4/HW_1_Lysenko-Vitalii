676ced4157cd19f92db496644fe36d9666e0527c
Vitalii Lysenko <balthazar.kain@gmail.com>
2018-05-04 00:19:55 +0300

    One Time Setup Update

---
+++ Calculator/Tests/OneTimeSetup.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using Calculator;
+using NUnit.Framework;
+
+namespace Tests
+{
+    [TestFixture]
+    public class OneTimeSetup
+    {
+        string phrase;
+        string Words;
+        [OneTimeSetUp]
+        public void OneTime()
+        {
+
+            phrase = "the best";
+            Words = "If you want to be the best...";
+        }
+
+        [OneTimeTearDown]
+        public void OnetimeTeardown()
+        {
+            phrase = null;
+
+        }
+
+        [Test]
+        public void AssertStringContain2()
+        {
+            Assert.That(phrase, Does.Contain(phrase), Words);
+        }
+
+        [Test]
+        public void AssertStringEnd2()
+        {
+            Assert.That(phrase, Does.EndWith("st"), Words);
+        }
+
+        [Test]
+        public void AssertStringStart2()
+        {
+            Assert.That(phrase, Does.StartWith("th"), Words);
+        }
+
+        [Test]
+        public void AssertIgnore2()
+        {
+            StringAssert.AreEqualIgnoringCase("The Best", phrase, Words);
+        }
+    }
+}

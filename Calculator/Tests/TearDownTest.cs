4fea54df6694120ce99791b12695779603d03b43
Vitalii Lysenko <balthazar.kain@gmail.com>
2018-05-04 00:19:14 +0300

    Tear Down Test Update

--- Calculator/Tests/TearDownTest.cs
+++ Calculator/Tests/TearDownTest.cs
@@ -7,39 +7,46 @@ namespace Tests
     [TestFixture, Description("SetUp and TearDown Testing")]
     public class TearDownTest
     {
-        ScientificCalculator a;
+        ScientificCalculator b;
         [SetUp]
         public void Construct()
         {
-            a = new ScientificCalculator();
+            b = new ScientificCalculator();
+        }
+        [TearDown]
+        public void CleanUp()
+        {
+            b = null;
+        }
+
+        [Test]
+        public void SinTesting()
+        {
+            Assert.That(b.Sin(30), Is.EqualTo(0.5), "Sin of 30 degree is equal to 0.5");
+        }
+
+        [Test]
+        public void CosTesting()
+        {
+            Assert.That(b.Cos(60), Is.EqualTo(0.5), "Cos of 60 degree is equal to 0.5");
         }
 
         [Test]
         public void SqrtTesting4()
         {
-            double d = 16;
-            double actual = a.Sqrt(d);
-            Assert.AreEqual(actual, 4);
+            Assert.That(b.Sqrt(16), Is.EqualTo(4), "Square root of 16 is equal to 4");
         }
 
         [Test]
         public void LogarithmTesting()
         {
-            double log = a.Logarithm(25, 5);
-            Assert.That(log, Is.EqualTo(2));
+            Assert.That(b.Logarithm(25, 5), Is.EqualTo(2));
         }
 
         [Test]
         public void DecimalLogarithmTesting()
         {
-            double d = a.Decimal_Logarithm(100);
-            Assert.That(d, Is.EqualTo(2));
-        }
-
-        [TearDown]
-        public void CleanUp()
-        {
-            a = null;
+            Assert.That(b.Decimal_Logarithm(100), Is.EqualTo(2));
         }
     }
 }

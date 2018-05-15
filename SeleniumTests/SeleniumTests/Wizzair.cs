1510d043bc80b1d9601d1b09f902305d94d4a6dc
Vitalii Lysenko <balthazar.kain@gmail.com>
2018-05-15 12:51:23 +0300

    HW4 Update 2

--- SeleniumTests/SeleniumTests/Wizzair.cs
+++ SeleniumTests/SeleniumTests/Wizzair.cs
@@ -32,68 +32,68 @@ namespace SeleniumTests
             
             WebDriverWait PageLoadWaiting = new WebDriverWait(driver, TimeSpan.FromSeconds(180));
             
-            //Place Choosing
-            IWebElement SearchDepartureStation = PageLoadWaiting.Until(Dep_St=> Dep_St.FindElement(By.Id("search-departure-station")));
+            //Route Choosing
+            IWebElement SearchDepartureStation = driver.FindElement(By.Id("search-departure-station"));
             SearchDepartureStation.Clear();
             SearchDepartureStation.SendKeys("Kiev");
             SearchDepartureStation.SendKeys(Keys.Enter);
+            string Departure_Stattion = SearchDepartureStation.Text;
 
             IWebElement DestinationPoint = driver.FindElement(By.Id("search-arrival-station"));
             DestinationPoint.Clear();
             DestinationPoint.SendKeys("Copenhagen");
             DestinationPoint.SendKeys(Keys.Enter);
+            string Destination_Point = DestinationPoint.Text;
 
             //Date Choosing
-            IWebElement Set_Out_Date = driver.FindElement(By.XPath("//td[not(class='is-disabled')]/button"));
-            Set_Out_Date.Click();
-                    
-             IWebElement DateOkButton = PageLoadWaiting.Until(Date_Ok_B=> Date_Ok_B.FindElement(By.XPath("//button[contains(@class,'shrinkable')]")));
-             DateOkButton.Click();
+            IWebElement Set_Out_Date = driver.FindElement(By.Id("search-departure-date"));
+            string Verified_Set_Out_Date = Set_Out_Date.Text;
 
-             //SearchButton
-             IWebElement SearchButton = driver.FindElement(By.XPath("//button[contains(@class, 'panel__cta-btn')]"));
+            //Passenger Field
+            IWebElement Pas_Field = driver.FindElement(By.Id("search-passenger"));
+            Pas_Field.Click();
+            IWebElement Pas_Field_Ok_Button = driver.FindElement(By.CssSelector("[class*=done-btn] > button"));
+            Pas_Field_Ok_Button.Click();
+
+            //SearchButton
+            IWebElement SearchButton = driver.FindElement(By.XPath("//button[contains(@class, 'panel__cta-btn')]"));
              SearchButton.Click();
 
              //New browser tab opening
              driver.SwitchTo().Window(driver.WindowHandles[1]);
 
-             //Date Verifying
-             IWebElement SelectedDate = PageLoadWaiting.Until(Date_Ver=> Date_Ver.FindElement(By.CssSelector(".js-selected.js-selectable > label > div > time")));
-             string DateActual = SelectedDate.Text;
-             string DateExpected = "Fri 14, May";
-             StringAssert.AreEqualIgnoringCase(DateExpected, DateActual, "Date should be set to Friday 14, May");
+            //Date Verification
+            IWebElement FlyingDate = PageLoadWaiting.Until(Flying_Date => Flying_Date.FindElement(By.CssSelector("td > time")));
+            string Correct_FlightDate = FlyingDate.Text;
+            Assert.That(Verified_Set_Out_Date, Does.Contain(Correct_FlightDate), "Flying Dates should be equal");
 
-             //Basic Option is displayed
-             IWebElement BasicOption = driver.FindElement(By.XPath("//p[contains (@class, 'header__title') and text() = 'Basic']"));
+            //Verifying the Departure and Destination points  
+            IWebElement SpecifiedRoute = driver.FindElement(By.XPath("//div[contains(@class,'outbound')]/div/address"));
+            string SpecifiedPath = SpecifiedRoute.Text;
+            Assert.That(SpecifiedPath, Does.Contain(Departure_Stattion), "Flying route should contain correct Departure station");
+            Assert.That(SpecifiedPath, Does.Contain(Destination_Point), "Flying route should contain correct Destination point");
+
+            //Verifying that  the flying date is selected
+            IWebElement SelectedDate = driver.FindElement(By.CssSelector(".js-selected.js-selectable > label > div > time"));
+            string DateActual = SelectedDate.Text;
+            Assert.AreEqual(Correct_FlightDate, DateActual);
+
+            //The Block of Basic Option and its price is displayed
+            IWebElement BasicOption = driver.FindElement(By.CssSelector("[class*=column--middle] div:first-of-type > label > div.rf-fare__price"));
              bool BasicOptionDisplayed = BasicOption.Displayed;
              Assert.True(BasicOptionDisplayed);
 
-            //Basic Prcie is displayed
-            IWebElement BasicElementPrice = driver.FindElement(By.XPath("//td[contains (@class, 'column--basic')]//strong"));
-             bool B_ElementPriceDisplayed = BasicElementPrice.Displayed;
-             Assert.True(B_ElementPriceDisplayed);
-
-            //WizzGo Option is displayed
-            IWebElement WizzGoOption = driver.FindElement(By.XPath("//p[contains (@class, 'header__title') and text() = 'WIZZ Go']"));
+            //The Block of WizzGo Option and its price is displayed
+            IWebElement WizzGoOption = driver.FindElement(By.CssSelector("[class*=column--middle] div:first-of-type > label > div.rf-fare__price"));
              bool WizzGoOptionDisplayed = WizzGoOption.Displayed;
              Assert.True(WizzGoOptionDisplayed);
 
-            //WizzGo Price is displayed
-            IWebElement WizzGoElementPrice = driver.FindElement(By.XPath("//td[contains (@class, 'column--middle')]//strong"));
-             bool WG_ElementPriceDisplayed = WizzGoElementPrice.Displayed;
-             Assert.True(WG_ElementPriceDisplayed);
-
-            // WizzPlus Option is displayed
-            IWebElement WizzPlusOption = driver.FindElement(By.XPath("//p[contains (@class, 'booking-flow') and text() = 'WIZZ Plus']"));
+            // The Block ofWizzPlus Option and its price is displayed
+            IWebElement WizzPlusOption = driver.FindElement(By.CssSelector("[class*=column--plus] div:first-of-type > label > div.rf-fare__price"));
              bool WizzPlusOptionDisplayed = WizzPlusOption.Displayed;
              Assert.True(WizzPlusOptionDisplayed);
 
-            //WizzPlus Price is displayed
-            IWebElement WizzPlusElementPrice = driver.FindElement(By.XPath("//td[contains (@class, 'column--plus')]//strong"));
-             bool WP_ElementPriceDisplayed = WizzPlusElementPrice.Displayed;
-             Assert.True(WP_ElementPriceDisplayed);
-
-             //TariffPlan choosing
+             //Tariff Plan choosing
              IWebElement Offer = driver.FindElement(By.XPath("//label[contains(@class, 'main')]"));
              Offer.Click();
 
@@ -101,7 +101,7 @@ namespace SeleniumTests
              ContinueButton.Click();
 
              //First Nmae, Last Name, Luggage
-             IWebElement FirstNameField = PageLoadWaiting.Until(FN_Field=> FN_Field.FindElement(By.Id("passenger-first-name-0")));
+             IWebElement FirstNameField = PageLoadWaiting.Until(First_Name_Field=> First_Name_Field.FindElement(By.Id("passenger-first-name-0")));
              FirstNameField.SendKeys("Vitalii");
              IWebElement LastNameField = driver.FindElement(By.Id("passenger-last-name-0"));
              LastNameField.SendKeys("Lysenko");
@@ -116,19 +116,11 @@ namespace SeleniumTests
             string El_Passenger = Passenger.Text;
             Assert.AreEqual("1 PASSENGER", El_Passenger, "1 Passenger should be set");
 
-            //Route Verifying
-            IWebElement SpecifiedPath = driver.FindElement(By.CssSelector("span.flight-info__stations"));
-             bool SpecifiedPathIsDisplayed = SpecifiedPath.Displayed;
-             Assert.That(SpecifiedPathIsDisplayed, Is.True, "Path <<Kiev - Zhulyany - Copenhagen>> should be displayed");
-             string ActualPath = SpecifiedPath.Text;
-             string ExpectedPath = "Kiev - Zhulyany - Copenhagen";
-             Assert.AreEqual(ExpectedPath, ActualPath, "Path should be set to <<Kiev - Zhulyany - Copenhagen>>");
-
              IWebElement ContinueButton2 = driver.FindElement(By.Id("passengers-continue-btn"));
              ContinueButton2.Click();
 
              //Sign In Form
-             IWebElement SignInForm = PageLoadWaiting.Until(Sign_F=> Sign_F.FindElement(By.Id("login-modal")));
+             IWebElement SignInForm= PageLoadWaiting.Until(Sign_In_Form=> Sign_In_Form.FindElement(By.Id("login-modal")));
              bool SignInFormDisplayed = SignInForm.Displayed;
              Assert.True(SignInFormDisplayed, "Signin should be displayed");
         }      
